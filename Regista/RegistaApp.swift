6bbbefccd2fb1a9367e6dc7a8d53c0ec0f5460db
Cenk <ckyz1007@gmail.com>
2022-04-03 16:09:50 +0300

    Initial Commit

---
+++ Regista/RegistaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistaApp.swift
+//  Regista
+//
+//  Created by Cenk Yavuz on 03/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct RegistaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
